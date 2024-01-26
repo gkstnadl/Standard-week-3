e0db29f542d8cf1cb3b366488e79fbe5ebc32f07
gkstnadl <gkstnadl12@naver.com>
2024-01-26 17:46:23 +0900

    resetFilter 수정

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -20,7 +20,7 @@ function FilterButtons({ students, setFilteredStudents }) {
       {/* 여기에 필터링 버튼들을 완성하세요. */}
       <button onClick={() => filterByAge(18)}>18세 이상</button>
       <button onClick={() => filterByGrade('A')}>A등급</button>
-      <button onClick={() => resetFilter()}>필터 초기화</button>
+      <button onClick={resetFilter}>필터 초기화</button>
     </div>
   );
 }
