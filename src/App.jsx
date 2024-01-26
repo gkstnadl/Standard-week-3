636417e8b02160cd7f7619e4b098d5a43ee0072c
gkstnadl <gkstnadl12@naver.com>
2024-01-26 16:16:14 +0900

    최희라 ex1 완료

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,21 @@ function App() {
   ];
 
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+  const filteredStudents = students.filter(student => student.age >= 18);
+
+  const handleClick = (age, grade) => {
+    alert(`나이 : ${age}, 성적 : ${grade}`)
+  };
 
   return (
     <div>
       <h1>학생 목록</h1>
       <ul>
+        {filteredStudents.map(student => (
+          <button key={student.name} onClick={()=>handleClick(student.age, student.grade)}>
+            {student.name}
+          </button>
+        ))}
         {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
         {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
       </ul>
